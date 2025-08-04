c24ddaa7a6c4ce9b278979429cc0b2bcd2bc5d86
Samuel W <68044712+Samuel-KW@users.noreply.github.com>
2025-08-03 23:01:12 -1000

    feat: basic snake handling

--- main_snake.c
+++ main_snake.c
@@ -1,7 +1,37 @@
 #include "os.c"
+#include "core.c"
+#include <stdlib.h>
+
+#define WIDTH 30
+#define HEIGHT 30
+#define MAX_SNAKE WIDTH *HEIGHT
+
+enum Direction
+{
+    UP,
+    DOWN,
+    LEFT,
+    RIGHT
+};
+
+typedef struct SnakeCell SnakeCell;
+struct SnakeCell
+{
+    int x;
+    int y;
+    SnakeCell* next;
+};
 
 void Entrypoint(void)
 {
-    // NOTE(poe): Put cross-platform code here
-    OS_Log("Hello, world!");
+    enum Direction direction = DOWN;
+
+    uint16_t seed = 0xB00B;
+
+    // TODO(sam): Don't spawn on edge
+    SnakeCell* head = (SnakeCell*) malloc(sizeof(SnakeCell));
+    head->x = lfsr_fibonacci(&seed) % WIDTH;
+    head->y = lfsr_fibonacci(&seed) % HEIGHT;
+    
+    SnakeCell* tail = head;
 }
