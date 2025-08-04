8ae61a814f9be5e829e00fc70a05a2315a5e0546
poetahto <epic.potat@gmail.com>
2025-08-04 08:14:51 -1000

    Improve build script with more options

--- compile.sh
+++ compile.sh
@@ -1,3 +1,26 @@
-gcc main_snake.c -o build/snake
-gcc main_poe_scratch.c -o build/poe_scratch
-gcc main_sam_scratch.c -o build/sam_scratch
+#!/bin/bash
+
+# NOTE(poe): Unpack CLI args (ensures that the -v "if" checks work)
+for arg in "$@"; do declare $arg='1'; done
+
+# NOTE(poe): Set defaults for unspecified settings
+if [ ! -v release ]; then debug=1; fi
+if [ ! -v snake ] && [ ! -v poe ] && [ ! -v sam ]; then all=1; fi
+
+if [ -v debug ]; then echo "[debug mode]"; fi
+if [ -v release ]; then echo "[release mode]"; fi
+
+gcc_common="-Werror -Wall -lX11"
+gcc_debug="gcc -g -Og $gcc_common"
+gcc_release="gcc -Ofast $gcc_common"
+
+if [ -v debug ]; then compile=$gcc_debug; fi
+if [ -v release ]; then compile=$gcc_release; fi
+
+mkdir -p build
+cd build
+if [ -v all ]; then snake=1 poe=1 sam=1; fi
+if [ -v snake ]; then echo "compiling snake"; $compile ../main_snake.c -o snake; fi
+if [ -v poe ]; then echo "compiling poe scratch"; $compile ../main_poe_scratch.c -o poe_scratch; fi
+if [ -v sam ]; then echo "compiling sam scratch"; $compile ../main_sam_scratch.c -o sam_scratch; fi
+cd ..
